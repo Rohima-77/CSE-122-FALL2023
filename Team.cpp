70f92f0bd4699e88dcf511364f582fc737b994d7
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-08-05 23:30:09 +0600

    Update Team.cpp

--- Team.cpp
+++ Team.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, p ,v ,t ,cnt = 0;
+    cin>>n;
+
+    while(n--)
+    {
+        cin >> p >> v >> t;
+        if((p + v + t)>=2)
+        {
+            cnt++;
+        }
+
+    }
+    cout << cnt <<endl;
+    return 0;
+}
 
