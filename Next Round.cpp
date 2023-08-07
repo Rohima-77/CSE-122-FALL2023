0c25074b6e1d34049a9da0b9550f3b6238e0c903
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-08-07 09:45:04 +0600

    Update Next Round.cpp

--- Next Round.cpp
+++ Next Round.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,k, count=0;
+    cin >> n >> k;
+    int nums[n];
+    for(int i=0;i<n;i++)
+        cin>>nums[i];
+    for(int j=0;j<n;j++)
+    {
+        if(nums[j]>=nums[k-1]&& nums[j]>0)
+        {
+            count++;
+        }
+
+    }
+    cout<<count;
+    return 0;
+}
 
