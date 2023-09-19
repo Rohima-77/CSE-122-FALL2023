919946e6fba0184be6acd587ecbdf9457373a934
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-09-19 23:48:09 +0600

    Update String task.cpp

--- String task.cpp
+++ String task.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string input,result ="";
+    cin >> input;
+
+    for(int i=0;i<input.size();i++)
+    {
+        char letter = tolower(input[i]);
+        if(letter == 'a'|| letter == 'o'|| letter == 'y'|| letter == 'e'|| letter == 'u'|| letter == 'i')
+        {
+            continue;
+        }
+        else
+        {
+            result += ".";
+            result += letter;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
 
