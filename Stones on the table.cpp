be87a740af18298fd1a4139f7aa5f1ad9271e235
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-09-24 09:07:27 +0600

    Update Stones on the table.cpp

--- Stones on the table.cpp
+++ Stones on the table.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int n , cnt = 0;
+    cin >> n;
+
+    string stoneColour;
+    cin >> stoneColour;
+
+    for(int i=0; i<n-1; i++)
+    {
+        if(stoneColour[i] == stoneColour[i+1])
+        {
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    return 0;
+}
