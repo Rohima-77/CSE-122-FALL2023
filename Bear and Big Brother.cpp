d2b51cd7445cccba580b9ec08a65e118485cbaa0
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-10-01 09:03:58 +0600

    Update Bear and Big Brother.cpp

--- Bear and Big Brother.cpp
+++ Bear and Big Brother.cpp
@@ -1,1 +1,17 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int l,b,cnt = 0;
+    cin >> l >> b;
+ 
+    while(l<=b)
+    {
+        l = l*3;
+        b = b*2;
+        cnt++;
+    }
+    cout << cnt << endl;
+    return 0;
+}
