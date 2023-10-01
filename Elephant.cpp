f8b53d1c027d0106c0086cea0d492fcea278a713
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2023-10-01 09:40:38 +0600

    Update Elephant.cpp

--- Elephant.cpp
+++ Elephant.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n , minimumstep;
+    cin >> n;
+ 
+    if(n % 5 == 0)
+    {
+        minimumstep = n / 5;
+    }
+    else
+    {
+        minimumstep = n / 5 + 1;
+    }
+    cout << minimumstep << endl;
+    return 0;
+}
 
